bb5421dbd905d1894393ea96e7a86428ea487a12
Caitlin Jordan <jordan.cait17@gmail.com>
2023-06-12 08:49:03 -0700

    completed lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe(){
+    function focused(){
+        console.log('Good!')
+    }
+    function blurred(){
+        console.log('Hey! Eyes on me!')
+    }
+    return (
+        <div>
+            <button onFocus={focused} onBlur={blurred}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe;
